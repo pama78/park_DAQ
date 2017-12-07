398523c36454b4263b75b35753ead08c182d07d1
pama78 <pavel.majer78@gmail.com>
2017-12-07 23:18:29 +0100

    update sdk a prvni pokusy o sync

--- app/application.c
+++ app/application.c
@@ -1,6 +1,7 @@
 #include <application.h>
 #include <usb_talk.h>
 #include <bc_usb_cdc.h>
+#include <bc_radio.h>
 
 //* Constants
 #define SR04_DATA_STREAM_SAMPLES 6
@@ -16,6 +17,7 @@ bc_led_t led;
 
 // Button instance
 bc_button_t button;
+uint16_t button_event_count = 0;
 
 //distance
   BC_DATA_STREAM_FLOAT_BUFFER(stream_buffer_distance_meter, SR04_DATA_STREAM_SAMPLES)
@@ -33,20 +35,23 @@ bc_button_t button;
 //#Handlers
 void button_event_handler(bc_button_t *self, bc_button_event_t event, void *event_param)
 {
+    bc_log_info("#button_event_handler initiated\r\n");
     (void) self;
-    (void) event_param;
+    uint16_t *event_count = (uint16_t *) event_param;
 
     if (event == BC_BUTTON_EVENT_PRESS)
     {
         bc_led_set_mode(&led, BC_LED_MODE_TOGGLE);
         bc_log_info("button pressed");
+        (*event_count)++;
+        bc_radio_pub_push_button(event_count);
     }
 }
 
 void proximity_sensor_hanler(bc_hc_sr04_event_t event, void *event_param)
 {
     float distance;
-    bc_log_info(" #proximity_sensor_hanler initiated\r\n");
+    bc_log_info("#proximity_sensor_hanler initiated\r\n");
 
     if (event == BC_HC_SR04_EVENT_UPDATE)
     {
@@ -65,28 +70,79 @@ void proximity_sensor_hanler(bc_hc_sr04_event_t event, void *event_param)
 
       if (sr04_current_sleep == SR04_LONG_SLEEP)
       {
-       bc_log_info("!!!!!sr04 about to set shortsleep  %i \r\n", SR04_LONG_SLEEP);
-       sr04_current_sleep=SR04_SHORT_SLEEP;
-       bc_log_info("!!!!!proximity_sensor_hanler -  set short sleep\r\n");
+       bc_log_info(" sr04 about to set shortsleep  %i \r\n", SR04_LONG_SLEEP);
+       sr04_current_sleep=SR04_SHORT_SLEEP;  
+       bc_log_info(" - proximity_sensor_hanler -  set short sleep\r\n");
+
+
        //pusobi problemy: bc_hc_sr04_set_update_interval(SR04_SHORT_SLEEP);
       }
     }
     else
     {
      sr04_loop_counter=0;
-     sr04_current_sleep=SR04_LONG_SLEEP;
-     bc_log_info("!!!!!proximity_sensor_hanler - about to set long sleep: %i \r\n", sr04_current_sleep);
+     sr04_current_sleep=SR04_LONG_SLEEP; 
+     bc_log_info(" - proximity_sensor_hanler - about to set long sleep: %i \r\n", sr04_current_sleep);
     //pusobi problemy: bc_hc_sr04_set_update_interval(SR04_LONG_SLEEP);
-     bc_log_info("!!!proximity_sensor_hanler - set long end: %i %i \r\n", sr04_loop_counter,SR04_DATA_STREAM_SAMPLES);
+     bc_log_info(" - proximity_sensor_hanler - set long end: %i %i \r\n", sr04_loop_counter,SR04_DATA_STREAM_SAMPLES);
     }
 }
 
+void send_message_to_server()
+{
+    bc_log_info("#send_message_to_server initiated\r\n");
+
+    uint8_t buffer[12];
+      buffer[4] = (int)(dist_average/10); //temporarily cannot be>255
+      bc_log_info("@sending: @dist=%i dm in buffer: %i \r\n",(int)(dist_average/10) , buffer[4]);
+      bc_radio_pub_buffer(buffer,sizeof(buffer));
+
+/*    buffer[0] = (int)my_device_address;  //id of client
+      buffer[1] = (int)my_device_address >> 8 ;  //id of client
+      buffer[2] = (int)my_device_address >> 16;  //id of client
+      buffer[3] = (int)my_device_address >> 24;  //id of client
+
+      DEBUG("@new serial: %i",(int)my_device_address);
+      DEBUG("@@atsha204_event_handler - detected serial (%i) \r\n", (int)serial );
+      DEBUG("@@radio init - detected serial (%i) \r\n", (int)my_device_address );
+      DEBUG("@@my_device_address:%i @@my_device_address_buff=%i \n\r",
+          (int) my_device_address, ((buffer[3] << 24) | (buffer[2] << 16) | (buffer[1] << 8) | buffer[0]));
+
+      buffer[5] = (int)(lux_average/lux_day_watermark*100); //percent of light
+      DEBUG("@@percent light=%i in buffer: %i\r\n",(int)(lux_average/lux_day_watermark*100), buffer[5]);
+      buffer[6] = (int)(battery_voltage*100) ; //milivolts
+      DEBUG("@@battery voltage=%i in buffer: %i \r\n",(int)(battery_voltage*100), buffer[6]);
+      buffer[7] = (int)(battery_percentage) ;
+      DEBUG("@@battery percentage=%i in buffer: %i \r\n",(int)(battery_percentage), buffer[7]);
+      buffer[8] = 255 ;
+      buffer[9] = 255 ;
+      buffer[10] = 255 ; //max //code all in one byte: occupied, night, run id
+
+
+  if ( bc_radio_pub_buffer(buffer,sizeof(buffer)) )
+  {
+    //testing data send
+    DEBUG("sent buffer orig data is: serial:%i dist:%0.0f lux:%0.0f batVolt:%0.2f batPerc:%0.2f \n\r",
+     (int)serial, dist_average/10, lux_average, battery_voltage, battery_percentage);
+    DEBUG("sent buffer is: serial:%i dist:%i lux:%i batVolt:%i batPerc:%i \n\r",
+     (buffer[3] << 24) | (buffer[2] << 16) | (buffer[1] << 8) | buffer[0],
+     buffer[4],buffer[5],buffer[6],buffer[7],buffer[8],buffer[9],buffer[10]);
+  }
+  else
+  {
+    DEBUG("failed to sent buffer: serial:%i dist:%i lux:%i batVolt:%i batPerc:%i \n\r",
+    (buffer[3] << 24) | (buffer[2] << 16) | (buffer[1] << 8) | buffer[0],
+    buffer[4],buffer[5],buffer[6],buffer[7],buffer[8],buffer[9],buffer[10]);
+}
+*/
+
+}
 
 
 void application_init(void)
 {
   bc_usb_cdc_init();
-  bc_log_info("application_init started\r\n");
+  bc_log_info("#application_init started\r\n");
 
     // Initialize LED
     bc_led_init(&led, BC_GPIO_LED, false, false);
@@ -103,11 +159,17 @@ void application_init(void)
     bc_hc_sr04_set_event_handler(proximity_sensor_hanler, NULL);
     // bc_hc_sr04_set_update_interval(SR04_UPDATE_INTERVAL_SECONDS * 1000);
     bc_hc_sr04_set_update_interval(2000);
+
+    //init radio
+     bc_log_info("radio pairing request\r\n");
+
+     bc_radio_pairing_request(FIRMWARE, VERSION);
+     bc_led_pulse(&led, 2000);
 }
 
 void application_task(void *param)
 {
-    bc_log_info("application_task started (%i)...\r\n", loop_counter );
+    bc_log_info("#application_task started (%i)...\r\n", loop_counter );
 
     //local variables
     loop_counter++;
@@ -115,5 +177,10 @@ void application_task(void *param)
      //get medians (lux+distance is global)
     if (bc_data_stream_get_median(&stream_distance_meter, &dist_average))  { bc_log_info (" -current distance median is: %0.0f \r\n", dist_average);}
 
+    bc_log_info("calling send_message_to_server\r\n");
+    send_message_to_server();
+
+     bc_log_info("set plan relative 1000\r\n");
+     bc_scheduler_plan_current_relative(1000);
 }
 
--- app/application.h
+++ app/application.h
@@ -1,6 +1,14 @@
 #ifndef _APPLICATION_H
 #define _APPLICATION_H
 
+#ifndef FIRMWARE
+#define FIRMWARE "generic-node"
+#endif
+
+#ifndef VERSION
+#define VERSION "vdev"
+#endif
+
 #include <bcl.h>
 #define TALK_OVER_CDC 1
 #pragma GCC diagnostic ignored "-Wunused-parameter"
